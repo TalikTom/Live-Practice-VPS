254f3b9030d81276225b8f5d6ebba562e76d8dd5
Luka Agic <agic.luke@gmail.com>
2023-02-15 16:57:47 +0100

    Add view counter to "protected" page

--- controllers/protected.php
+++ controllers/protected.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
 
+$counter = $_SESSION['counter'] ?? 0;
+$counter = $counter + 1;
+$_SESSION['counter'] = $counter;
+
+$message = 'Page view: ' . $counter;
+
 if(!$_SESSION['auth']){
     header('location: /');
 }
--- views/protected.view.php
+++ views/protected.view.php
@@ -7,9 +7,12 @@
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
+            <title>Protected</title>
         </head>
         <body>
+        <p>
+            <?= $message ?>
+        </p>
         <a href="/login">Log off</a>
         </body>
         </html>
