99fba5ea26b77c91e1ea754241c9614cf05cb9dc
Luka Agic <agic.luke@gmail.com>
2023-02-22 17:21:24 +0100

    Add validation for register inputs

--- controllers/register.php
+++ controllers/register.php
@@ -1,6 +1,15 @@
 <?php
 
-
+// Now we check if the data was submitted, isset() function will check if the data exists.
+if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
+    // Could not get the data that should have been sent.
+    exit('Please complete the registration form!');
+}
+// Make sure the submitted registration values are not empty.
+if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
+    // One or more values are empty.
+    exit('Please complete the registration form');
+}
 
 
 require 'views/register.view.php';
